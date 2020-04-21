ce642c87018f4d152e75cea424cdcd7ff1da1c7f
Ian Travers <mr.itpe@gmail.com>
2020-04-21 09:33:23 +0300

    Update settings profile page (WIP)

--- resources/views/settings/profile.blade.php
+++ resources/views/settings/profile.blade.php
@@ -10,7 +10,34 @@
                 <div class="card">
                     <div class="card-body">
                         <h3>{{ __('settings.profile') }}</h3>
-                        {{ $user->name }}
+                        <hr>
+                        <form action="{{ route('home') }}">
+                            <div class="form-group row">
+                                <label for="nickname" class="col-md-4 col-form-label text-md-right">{{ __('auth.nickname') }}</label>
+                                <div class="col-md-6">
+                                    <input id="nickname" type="text" class="form-control @error('nickname') is-invalid @enderror" name="nickname" value="{{ old('nickname') }}" required autocomplete="nickname" autofocus>
+
+                                    @error('nickname')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('auth.name') }}</label>
+                                <div class="col-md-6">
+                                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" autocomplete="name">
+
+                                    @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
