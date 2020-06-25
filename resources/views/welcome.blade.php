cea940aad94e8fef0f36b7c457a646a38dc5175c
Ian Travers <mr.itpe@gmail.com>
2020-06-25 14:33:27 +0300

    Separate content of the root page (quest || user)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,37 +1,47 @@
 <x-frontend-layout :title="config('app.name')">
     <div class="container">
-        <div class="row">
-            <div class="col-md-8 position-relative">
-                <div
-                    class="text-light position-absolute"
-                    style="top: calc(50% - 6em); height: 12em;"
-                >
-                    <div class="display-4 font-weight-bold mb-2">Let's play NFS Underground</div>
-                    <p class="h3">
-                        The simple way to get online racing. You can play here with your friends and unfamiliar players.</p>
-                    <p class="h3">
-                        Hey! And if a tourney is scheduled, don't forget to take part in it.
-                    </p>
+
+        @guest
+            <div class="row">
+                <div class="col-md-8 position-relative">
+                    <div
+                        class="text-light position-absolute"
+                        style="top: calc(50% - 6em); height: 12em;"
+                    >
+                        <div class="display-4 font-weight-bold mb-2">Let's play NFS Underground</div>
+                        <p class="h3">
+                            The simple way to get online racing. You can play here with your friends and unfamiliar
+                            players.</p>
+                        <p class="h3">
+                            Hey! And if a tourney is scheduled, don't forget to take part in it.
+                        </p>
+                    </div>
                 </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card">
-                    <div class="card-body">
-                        <form method="post" action="{{ route('register') }}">
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <form method="post" action="{{ route('register') }}">
 
-                            @csrf
-                            @include('frontend.partials.register-form')
+                                @csrf
+                                @include('frontend.partials.register-form')
 
-                            <div class="mb-0 mt-3 text-center">
-                                <button type="submit" class="btn btn-success btn-lg btn-block">
-                                    {{ __('auth.register') }}
-                                </button>
-                            </div>
-                        </form>
+                                <div class="mb-0 mt-3 text-center">
+                                    <button type="submit" class="btn btn-success btn-lg btn-block">
+                                        {{ __('auth.register') }}
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
+        @endguest
+
+        @auth
+            <div class="container text-info">
+                <h3><b>{{ auth()->user()->nickname }}</b> overall score information</h3>
+            </div>
+        @endauth
     </div>
 </x-frontend-layout>
