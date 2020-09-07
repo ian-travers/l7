90aef74421cc56d7d6a6d972336f43d50a3b04bc
Ian Travers <mr.itpe@gmail.com>
2020-09-07 15:35:49 +0300

    Extract _commentJs

--- resources/views/frontend/news/show.blade.php
+++ resources/views/frontend/news/show.blade.php
@@ -46,25 +46,5 @@
             </div>
         @endguest
     </div>
-
-    @section('script')
-        <script type="text/javascript">
-            $(document).on('click', '#comments .comment-reply', function () {
-                let link = $(this);
-                let form = $('#reply-block');
-                let comment = link.closest('.comment-item');
-                $('#parent-id').val(comment.data('id'));
-                form.detach().appendTo(comment.find('.reply-block:first'));
-                document.getElementById('cancel-reply').style.display = 'inline-block';
-                return false;
-            });
-            $(document).on('click', '#comments #cancel-reply', function () {
-                let form = $('#reply-block');
-                $('#parent-id').removeAttr('value');
-                form.detach().appendTo('#root-level');
-                document.getElementById('cancel-reply').style.display = 'none';
-                return false;
-            });
-        </script>
-    @endsection
+    @include('frontend._commentJs')
 </x-frontend-layout>
